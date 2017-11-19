569ea3626a631f31dc56fc0cbcf6af4d7644777c
maria.katsourani <maria.katsourani@outlook.com>
2017-11-19 23:00:05 +0100

    Fetch repos from GH

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,40 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import api from './../utils/api';
 
-class Popular extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedLanguage: 'All'
-    };
+function SelectLanguage(props) {
+    const languages = ['All', 'Javascript', 'Java', 'Ruby', 'CSS', 'Python'];
 
-    this.updateLanguage = this.updateLanguage.bind(this);
-  } 
-
-  updateLanguage(lang) {
-    this.setState({
-      selectedLanguage: lang
-    });
-  }
-
-  render() {
-    var languages = ['All', 'Javascript', 'Java', 'Ruby', 'CSS', 'Python'];
-    
     return (
       <ul className="languages">
         {languages.map(lang => {
           return (
             <li 
-              style={lang === this.state.selectedLanguage ? { color: '#d0021b'}: null}
+              style={lang === props.selectedLanguage ? { color: '#d0021b'}: null}
               key={lang} 
-              onClick={this.updateLanguage.bind(null, lang)}>
+              onClick={props.onSelect.bind(null, lang)}>
                 {lang}
             </li>
           )
         })}
-      </ul>
+    </ul>
+  )
+}
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  } 
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  updateLanguage(lang) {
+    this.setState({
+      selectedLanguage: lang,
+      repos: null
+    });
+
+    api.fetchPopularRepos(lang)
+    .then(repos => {
+      this.setState(() => {
+        return {
+          repos: repos
+        }
+      })
+    })
+  }
+
+  render() {    
+    return (
+      <div>
+        <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage} />
+        {JSON.stringify(this.state.repos, null, 2)}
+      </div>
     )
   }
 
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
 }
 
 export default Popular;
