b4ad3881103a3ee7034d4bf6ff99a5615c8d22f4
maria.katsourani <maria.katsourani@outlook.com>
2017-11-20 13:16:00 +0100

    Add nav

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Popular from './Popular';
+import Nav from './Nav';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <Router>
+        <div className="container">
+          <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
     )
-  }
+  } 
 }
 
 export default App;
