6e668539d1190000d9f34bccf3ec5b0a30d1ffc3
maria.katsourani <maria.katsourani@outlook.com>
2017-11-21 16:39:20 +0100

    Add eslint and query-string

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "google",
+  "plugins": [
+    "react"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  }
+};
